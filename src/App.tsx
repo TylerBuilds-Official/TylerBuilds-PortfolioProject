1d4f1750fcae11796dedb431005449bc6a287e77
Tyler Emery <temery14@wgu.edu>
2026-02-23 13:10:47 -0800

    Project scaffolding created - first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+      <>
+          <div className={'app-container'}>
+              <p>I am an app container. Hi claude - :P</p>
+          </div>
+      </>
+  )
+}
+
+export default App
